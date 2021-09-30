5fc451d4c46cb6bfde05ad37b04f37c1ae4bfcf4
henk <s344104@oslomet.no>
2021-09-30 13:28:22 +0200

    Working right now

--- app/src/main/java/com/example/mappe2_s344104_s344045/DBHandler.java
+++ app/src/main/java/com/example/mappe2_s344104_s344045/DBHandler.java
@@ -4,12 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DBHandler {
+public class DBHandler extends SQLiteOpenHelper {
     static String TABLE_RESTAURANTS = "Restauranter";
     static String KEY_ID = "_ID";
     static String KEY_NAME = "Navn";
