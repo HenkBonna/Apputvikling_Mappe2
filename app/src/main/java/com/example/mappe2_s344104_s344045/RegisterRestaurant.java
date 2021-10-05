9485dbb53a3324ede433db1a50edd5e00b985dc1
KapteinKS <sander.toien@outlook.com>
2021-10-05 13:33:33 +0200

    more design work

--- app/src/main/java/com/example/mappe2_s344104_s344045/RegisterRestaurant.java
+++ app/src/main/java/com/example/mappe2_s344104_s344045/RegisterRestaurant.java
@@ -1,6 +1,7 @@
 package com.example.mappe2_s344104_s344045;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +10,9 @@ public class RegisterRestaurant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_restaurant);
+    }
+
+    public void add(View view) {
+        //TODO implement method
     }
 }
