d64af889e0f5b62122af30aea1385c6fb3548b43
henk <s344104@oslomet.no>
2021-10-05 13:38:19 +0200

    ListRestaurants work

--- app/src/main/java/com/example/mappe2_s344104_s344045/ListFriends.java
+++ app/src/main/java/com/example/mappe2_s344104_s344045/ListFriends.java
@@ -8,6 +8,6 @@ public class ListFriends extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_friends);
+        setContentView(R.layout.activity_list);
     }
 }
