d14e9f84f49dbdfc9d968ed2087c99045301dfd8
henk <s344104@oslomet.no>
2021-10-26 13:12:56 +0200

    Started work on ContentProvider

--- app/src/main/java/com/example/mappe2_s344104_s344045/Restaurant.java
+++ app/src/main/java/com/example/mappe2_s344104_s344045/Restaurant.java
@@ -1,11 +1,18 @@
 package com.example.mappe2_s344104_s344045;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
-public class Restaurant {
+public class Restaurant extends ContentProvider {
     @PrimaryKey(autoGenerate = true)
     private Long _ID;
     @ColumnInfo(name = "restaurant_name")
@@ -18,6 +25,39 @@ public class Restaurant {
     private String type;
 
     public Restaurant (){}
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
 
     public Restaurant(String name, String address, String phone, String type) {
         this.name = name;
